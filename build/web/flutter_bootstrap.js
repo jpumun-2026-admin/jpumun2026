76b6def536148f30aab7d9d8f963073866be2369
rehaan <97499666+DatDevSteve@users.noreply.github.com>
2026-08-06 19:43:51 +0530

    please make this work i swear

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "776605713" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "303644904" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
